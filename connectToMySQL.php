50499aba4c5cec74c02d33b6bd9934711fed7fa6
mike <chennanbang@gmail.com>
2017-09-21 14:44:55 +0800

    使用 php 讀取 mysql 資料

--- connectToMySQL.php
+++ connectToMySQL.php
@@ -13,9 +13,10 @@
         $host = "localhost";//請看 主機上的 /etc/hosts
         $user = "homestead";//homestead 內建的帳號密碼
         $password = "secret";
+        $database = "test";//還需要 use database
 
         //執行連線到資料庫的動作, 並將回傳的東西存放到 $dbLink 中
-        $dbLink = new mysqli($host, $user, $password);
+        $dbLink = new mysqli($host, $user, $password, $database);//還需要 use database
 
         //檢查是否連線錯誤
         if($dbLink->connect_error)
@@ -25,6 +26,23 @@
         }
         else
             echo "連線成功<br>~";
+
+        $sql = "SELECT * FROM students";//要執行的 sql 語法
+        $result = $dbLink->query($sql);//叫 mysql 執行
+
+        var_dump($result);//列出除錯用的資訊
+
+        if( $result->num_rows > 0 )//檢查是否有資料
+        {
+            while( $row = $result->fetch_array() )//將資料取出 列印
+            {
+                echo "name:".$row["name"].
+                     ", gender:".$row["gender"].
+                     ", address:".$row["addressArea"].$row["addressDetail"];
+            }
+        }
+        else
+            echo "no data";
     ?>
 </body>
 </html>
