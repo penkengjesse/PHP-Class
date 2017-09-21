5174b0b61d4c41b7bff75a7e8423bb6b6dc6cd33
mike <chennanbang@gmail.com>
2017-09-21 14:02:34 +0800

    補充 mysql 連線帳號密碼, 註解物件/程序導向

--- connectToMySQL.php
+++ connectToMySQL.php
@@ -9,7 +9,7 @@
     <?php
         echo "測試連結資料庫...";
 
-        //註解:變數 用來設定連線資料
+        //註解:變數 用來設定連線資料 在命令列上可以打 mysql --user="homestead" --password="secret" 來登入
         $host = "localhost";//請看 主機上的 /etc/hosts
         $user = "homestead";//homestead 內建的帳號密碼
         $password = "secret";
@@ -20,7 +20,8 @@
         //檢查是否連線錯誤
         if($dbLink->connect_error)
         {
-            die("連線錯誤:".$dbLink->connect_error);
+//            die("連線錯誤:".$dbLink->connect_error);//物件導向
+            die("連線錯誤:".mysqli_connect_error());//程序導向
         }
         else
             echo "連線成功<br>~";
