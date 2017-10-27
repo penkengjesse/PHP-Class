07b7c6b462445305b5b3b75c1c8363dd49c7070d
DESKTOP-H6KFVDQ\yvtc <penkengjesse@gmail.com>
2017-10-27 09:04:33 +0800

    CONNECT TO MYSQL
    Signed-off-by: DESKTOP-H6KFVDQ\yvtc <penkengjesse@gmail.com>

--- connectToMySQL.php
+++ connectToMySQL.php
@@ -7,28 +7,39 @@
 <body>
     <h1>連接到 mysql</h1>
     <?php
-        echo "測試連結資料庫...";
+
+    include 'api.php';
+
+
+        echo "測試連結test資料庫.test..";
+
+
+
 
         //註解:變數 用來設定連線資料 在命令列上可以打 mysql --user="homestead" --password="secret" 來登入
-        $host = "localhost";//請看 主機上的 /etc/hosts
-        $user = "homestead";//homestead 內建的帳號密碼
-        $password = "secret";
-        $database = "test";//還需要 use database
+ //       $host = "localhost";//請看 主機上的 /etc/hosts
+  //      $user = "homestead";//homestead 內建的帳號密碼
+ //       $password = "secret";
+  //      $database = "phpMySQL";//還需要 use database
 
-        //執行連線到資料庫的動作, 並將回傳的東西存放到 $dbLink 中
-        $dbLink = new mysqli($host, $user, $password, $database);//還需要 use database
+ //       //執行連線到資料庫的動作, 並將回傳的東西存放到 $dbLink 中
+ //       $dbLink = new mysqli($host, $user, $password, $database);//還需要 use database
 
         //檢查是否連線錯誤
-        if($dbLink->connect_error)
-        {
+ //       if($dbLink->connect_error)
+ //       {
 //            die("連線錯誤:".$dbLink->connect_error);//物件導向
-            die("連線錯誤:".mysqli_connect_error());//程序導向
-        }
-        else
-            echo "連線成功<br>~";
+ //           die("連線錯誤:".mysqli_connect_error());//程序導向
+ //       }
+  //      else
+  //          echo "連線成功<br>~";
+
+
+$dbLink = connectToMySQL();
+
 
         //新增一筆資料
-        $sql = "INSERT INTO students VALUES('jack','m','XingZhu','ZhongShan Rd.')";
+        $sql = "INSERT INTO students VALUES('mary','f','tainan','ZhongShan Rd.')";
 
         if( $dbLink->query( $sql) )
             echo "成功新增資料";
